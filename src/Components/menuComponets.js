34b7e244e10e97da85686522f5cd707cae17b662
Gabriel Gualteros <Roblox4@gmail.com>
2021-05-13 21:47:30 -0400

    add assignment 1 components

--- src/Components/menuComponets.js
+++ src/Components/menuComponets.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from "./DishdetailComponent";
 
 class Menu extends Component {
 
@@ -9,66 +10,31 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
-        console.log('Menu Component constructor is invoked');
     }
-
-    componentDidMount() {
-        console.log('Menu Component did mount constructor is invoked');
-
-    }
-
     onDishSelect(dish) {
         this.setState({ selectedDish: dish });
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle> {dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
+   render() {
+       const menu = this.props.dishes.map((dish) => {
+           return (
+               <div key={dish.id} className="col-12 col-md-5 m-1"> 
+                <Card onClick={() => this.onDishSelect(dish)}>
+                    <CardImg className="img-fluid" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
                 </Card>
-             );
-        }
-        else {
-            return(
-                <div></div>
-            )
-        }
-    }
+               </div>
+           );
+       });
 
-    render() {
-
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle> {dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
-        console.log('Menu Component constructor is invoked');
-
-
-        return ( 
-            <div className="container">
-                <div className="row">
-                        {menu}
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
-            </div>
-        );
-    }
+       return (
+           <div className="container">
+               <div className="row">{menu}</div>
+               <DishDetail dish={this.state.selectedDish} />
+           </div>
+       );
+   }
 }
-
 export default Menu;
